42d1f5d16dffc26617008241902e7213a06a0a15
Ulrich Lukas <opensource@lukas-elektronik.de>
2025-12-26 10:44:22 +0100

    Refactor done

--- src/energy_modulator/api/local_logger.py
+++ src/energy_modulator/api/local_logger.py
@@ -6,7 +6,6 @@ import logging
 from energy_modulator.conf.energy_modulator_config import LocalLoggerConfig as conf
 from energy_modulator.store import EnergyModulatorStore
 
-logger = logging.getLogger(__name__)
 
 class LocalLogger:
     """Log output of meter active power readings for total, L1, L2 and L3.
@@ -17,12 +16,21 @@ class LocalLogger:
 
     def __init__(self, store: EnergyModulatorStore) -> None:
         """Initialize LocalLogger."""
+        # Application data store
         self.store = store
+        # Logger configuration
+        self.logger = logging.Logger(__name__)
+        console_handler = logging.StreamHandler()
+        formatter = logging.Formatter(fmt="%(asctime)s, %(message)s", datefmt="%Y-%m-%d, %H:%M:%S")
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+
 
     async def run_forever(self) -> None:
         """(Supposedly) forever running co-routine for power output logger."""
+        # CSV header: Component, Date, Time, Measurement, Unit, Channel, Channel, Channel, Channel
+        msg = "Active Power, W, Total, L1, L2, L3, %5.0f, %5.0f, %5.0f, %5.0f"
         while True:
             await asyncio.sleep(conf.LOG_INTERVAL)
-            p_tot, p_l1, p_l2, p_l3 = await self.store.get()
-            msg = f", Active Power, W, tot,L1,L2,L3, {p_tot:5.0f}, {p_l1:5.0f}, {p_l2:5.0f}, {p_l3:5.0f}"
-            logger.info(msg)
+            p_tot, p_l1, p_l2, p_l3 = await self.store.get_power()
+            self.logger.info(msg, p_tot, p_l1, p_l2, p_l3)
--- src/energy_modulator/api/sma_em_receiver.py
+++ src/energy_modulator/api/sma_em_receiver.py
@@ -5,12 +5,10 @@
 import asyncio
 import logging
 import socket
-from typing import cast
 
 from energy_modulator.store import EnergyModulatorStore
 from energy_modulator.conf.energy_modulator_config import SmaEmReceiverConfig as conf
 from energy_modulator.api.sma_em_protocol import SmaEmProtocol
-from include.sma_em.speedwiredecoder import decode_speedwire # type: ignore
 
 
 logger = logging.getLogger(__name__)
@@ -32,22 +30,6 @@ class SmaEmReceiver:
     def __init__(self, store: EnergyModulatorStore) -> None:
         """Initialize SmaEmReceiver."""
         self.store = store
-
-
-    async def get_power(self) -> tuple[float, float, float, float]:
-        """Return total power (power sum), power L1, power L2 and power L3."""
-        emparts = cast("dict[str, float]", await self.get_readings())
-        power_l1 = emparts["p1consume"] - emparts["p1supply"]
-        power_l2 = emparts["p2consume"] - emparts["p2supply"]
-        power_l3 = emparts["p3consume"] - emparts["p3supply"]
-        power_sum = power_l1 + power_l2 + power_l3
-        return power_sum, power_l1, power_l2, power_l3
-
-
-    async def get_readings(self) -> dict[str, float|int|str]:
-        """Get meter read-out values, returned as a dict."""
-        bytes_in = await self.store.get()
-        return cast("dict[str, float|int|str]", decode_speedwire(bytes_in))
 
 
     async def run_forever(self) -> None:
